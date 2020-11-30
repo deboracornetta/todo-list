cb79c434e16661b9dae02549d3ab344024bf817f
Debora Cornetta <cornettaintech@gmail.com>
2020-11-30 15:42:23 -0300

    Fixing flake8 requirements

--- application/todo/settings.py
+++ application/todo/settings.py
@@ -85,22 +85,13 @@ DATABASES = {
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': '''django.contrib.auth.password_validation.
-        UserAttributeSimilarityValidator''',
-    },
-    {
-        'NAME': '''django.contrib.auth.password_validation.
-        MinimumLengthValidator''',
-    },
-    {
-        'NAME': '''django.contrib.auth.password_validation.
-        CommonPasswordValidator''',
-    },
-    {
-        'NAME': '''django.contrib.auth.password_validation.
-        NumericPasswordValidator''',
-    },
+    {"NAME": f"django.contrib.auth.password_validation.{name}"}
+    for name in [
+        "UserAttributeSimilarityValidator",
+        "MinimumLengthValidator",
+        "CommonPasswordValidator",
+        "NumericPasswordValidator"
+    ]
 ]
 
 
@@ -122,3 +113,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+#    {
+#        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+#    },
+#    {
+#        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+#    },
+#    {
+#        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+#    },
+#    {
+#        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+#    },
